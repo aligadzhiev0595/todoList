569468e5385768ea711cf4636196b99a2ad26f53
aligadzhiev0595 <aligadzhiev0595@gmail.com>
2022-07-28 16:16:47 +0300

    fixForm

--- src/components/TodoForm.tsx
+++ src/components/TodoForm.tsx
@@ -1,26 +1,37 @@
 import { useAppDispatch, useAppSelector } from "../hooks/hooks";
 import { addTodo, inputHandler } from "../redux/slices/todoSlice";
 import { IoIosArrowDown } from "react-icons/io";
+import React from "react";
 
 const TodoForm: React.FC = () => {
   const dispatch = useAppDispatch();
   const input = useAppSelector((s) => s.todos.input);
 
   const actionHandler = () => {
-    if(input.length !== 0) dispatch(addTodo(input));
+    if (input.length !== 0) dispatch(addTodo(input));
+    dispatch(inputHandler(""));
+  };
+
+  const formHandler = (e: any) => {
+    e.preventDefault();
+    if (input.length !== 0) {
+      dispatch(addTodo(input));
+    }
     dispatch(inputHandler(""));
   };
 
   return (
-    <>
+    <form onSubmit={formHandler}>
       <input
         className="input"
         value={input}
-        onChange={(e) => dispatch(inputHandler(e.target.value))}
+        onChange={(e: React.ChangeEvent<HTMLInputElement>) =>
+          dispatch(inputHandler(e.target.value))
+        }
         placeholder="What needs to be done"
       />
       <IoIosArrowDown className="btn" onClick={actionHandler} />
-    </>
+    </form>
   );
 };
 
